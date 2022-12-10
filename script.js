8f81f82cb2b15e0c9813d94472d13c4197a41e47
anikateagrawal <anikateag7316@gmail.com>
2022-12-10 14:36:06 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-const fs=require("fs");
-fs.readFile("./books/AliceInWonderland.txt","utf-8",file);
 
-function file(e,data){
-    console.log(data);
+
+(Array.from(document.getElementsByTagName("a"))).forEach((a)=>{
+    console.log("assd");
+    a.addEventListener("click",book);
+});
+
+
+function book(e){
+    e.preventDefault();
+    console.log(e.target.id);
+    if(e.target.id=="alice"){
+        read("./books/AliceInWonderland.txt","Alice In WonderLand");
+    }
+    if(e.target.id=="hyde"){
+        read("./books/JekyllAndHyde.txt","Jekyll And Hyde");
+    }
+    if(e.target.id=="lord"){
+        read("./books/LOTR.txt","Lord Of The Rings");
+    }
 }
+function read(url,title){
+    var client = new XMLHttpRequest();
+client.open('GET', url);
+client.onreadystatechange = function() {
+  var t=client.responseText;
+  document.getElementById("bhead").innerText=title;
+  document.getElementById("btext").innerText=t;
+}
+client.send();
+}
+
